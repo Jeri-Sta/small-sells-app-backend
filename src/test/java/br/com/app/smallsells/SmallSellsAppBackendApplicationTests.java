e45fbd4c0a2b37aaa075f9e4a0199e9871412a57
jeriel.starosky <jerielstarosky@hotmail.com>
2024-03-22 18:51:11 -0300

    #1 criado repositório spring

---
+++ src/test/java/br/com/app/smallsells/SmallSellsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.app.smallsells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSellsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
